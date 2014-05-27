cf8711e10eff1c7a75117b8c1fc6324d9be41c63
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-05-27 11:39:46 -0300

    Update ctrl2.js

--- scripts/controller/ctrl2.js
+++ scripts/controller/ctrl2.js
@@ -1,6 +1,5 @@
 define(['js/app'], function (app) {
     app.register.controller('View1Ctrl', function ($scope) {
         $scope.message = "Message from View1Ctrl"; 
-        alert();
     });
 }); 
